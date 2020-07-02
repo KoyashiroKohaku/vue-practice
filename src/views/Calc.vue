101099681f04aac741772d43bd5672bcaf47325f
Koyashiro Kohaku <develop@koyashiro.net>
2020-07-02 18:14:00 +0900

    implement a validation

--- src/views/Calc.vue
+++ src/views/Calc.vue
@@ -1,32 +1,40 @@
 <template>
   <v-container>
-    <v-col cols="12" sm="6">
-      <v-text-field label="Left Value" single-line v-model.number="leftValue" />
-    </v-col>
+    <v-form v-model="isValid">
+      <v-col cols="12" sm="6">
+        <v-text-field
+          label="Left Value"
+          single-line
+          v-model.number="leftValue"
+          :rules="inputRules"
+        />
+      </v-col>
 
-    <v-col class="d-flex" cols="12" sm="6">
-      <v-select
-        label="Calc Type"
-        :items="types"
-        v-model.number="calcType"
-      ></v-select>
-    </v-col>
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select
+          label="Calc Type"
+          :items="types"
+          v-model.number="calcType"
+        ></v-select>
+      </v-col>
 
-    <v-col cols="12" sm="6">
-      <v-text-field
-        label="Right Value"
-        single-line
-        v-model.number="rightValue"
-      />
-    </v-col>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          label="Right Value"
+          single-line
+          v-model.number="rightValue"
+          :rules="inputRules"
+        />
+      </v-col>
 
-    <v-col cols="12" sm="6">
-      =
-    </v-col>
+      <v-col cols="12" sm="6">
+        =
+      </v-col>
 
-    <v-col cols="12" sm="6">
-      <v-text-field label="Result" single-line v-model.number="resultValue" />
-    </v-col>
+      <v-col cols="12" sm="6">
+        <v-text-field label="Result" single-line v-model.number="resultValue" />
+      </v-col>
+    </v-form>
   </v-container>
 </template>
 
@@ -44,19 +52,28 @@ export default Vue.extend({
   name: "Calc",
   data() {
     return {
+      isValid: false,
       types: [
         { text: "+", value: CalcType.Addition },
         { text: "-", value: CalcType.Subtraction },
         { text: "*", value: CalcType.Multiplication },
         { text: "/", value: CalcType.Division }
       ],
-      leftValue: 0,
-      rightValue: 0,
-      calcType: CalcType.Addition
+      leftValue: "",
+      rightValue: "",
+      calcType: CalcType.Addition,
+      inputRules: [
+        (v: string) => v.length !== 0 || "Value is required",
+        (v: string) => Number.isFinite(Number(v)) || "Value is not a number"
+      ]
     };
   },
   computed: {
-    resultValue(): number {
+    resultValue(): number | null {
+      if (!this.isValid) {
+        return null;
+      }
+
       switch (this.calcType) {
         case CalcType.Addition:
           return this.leftValue + this.rightValue;
@@ -67,7 +84,7 @@ export default Vue.extend({
         case CalcType.Division:
           return this.leftValue / this.rightValue;
         default:
-          return 0;
+          return null;
       }
     }
   }
